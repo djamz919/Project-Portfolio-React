ad4e5a923551e829023f7bef2ca06abd18f7d78c
Daryll M <djtm97@gmail.com>
2022-01-17 21:01:39 -0500

    first attempts at App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
 import Nav from './components/Navigation';
+import About from './components/About';
 import Footer from './components/Footer';
-import Header from './components/Header';
-import Project from './components/Project';
-import Contact from './components/Contact';
+// import Header from './components/Header';
+// import Project from './components/Project';
+// Import Skill from './components/Skill';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [categories] = useState([
+    { name: 'projects', description: 'display of projects' },
+    { name: 'skills', descriptions: 'list of credentials and skills'}
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+        <Footer></Footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import coverImage from '../../assets/cover/cover-image.jpg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <div className="my-2">
+        <p>
+        Daryll is currently a business analyst based in the New York Metro area with experience in user-acceptance testing, automation testing, and the cloud.
+        He graduated with a B.S. in Chemical Engineering with highest honors from Rutgers University and is currently enrolled in Rutgers Full Stack Coding Bootcamp.
+        He aspires to be an effective full-stack developer proficient in current technologies. Outside of work, Daryll enjoys traveling, learning languages, playing instruments, and spending time with family and friends.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
