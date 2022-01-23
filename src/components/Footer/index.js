61234821e1f63384d8ceed2f52676ec53cb4780d
Daryll M <djtm97@gmail.com>
2022-01-22 22:49:08 -0500

    add pics, css to skills

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
+
 function Footer() {
     return (
         <footer id="footer">
-        <div id="Contact" class="media">
-            <ul>
-                <li><a target="_blank" rel="noreferrer" href="https://github.com/djamz919" class="fab fa-github" aria-hidden="true"> </a></li>
-                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/daryll-munoz/" class="fab fa-linkedin" aria-hidden="true"> </a>
-                </li>
-                <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/djmz919/" class="fab fa-instagram" aria-hidden="true"> </a></li>
-                <li><a href="mailto:djtm97@gmail.com" class="fas fa-envelope" aria-hidden="true"> </a></li>
-            </ul>
-        </div>
-        <div class="copyright">
-            &copy; 2022 DARYLL MUNOZ
-        </div>
-    </footer>
+            <div id="Contact" class="media">
+                <ul>
+                    <li><a target="_blank" rel="noopener" href="https://github.com/djamz919" class="fab fa-github"></a></li>
+                    <li><a target="_blank" rel="noopener" href="https://www.linkedin.com/in/daryll-munoz/" class="fab fa-linkedin"></a></li>
+                    <li><a target="_blank" href="https://www.instagram.com/djmz919/" class="fab fa-instagram"></a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                &copy; 2022 DARYLL MUNOZ
+            </div>
+        </footer>
     );
 }
 
