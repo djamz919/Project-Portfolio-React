bcd99b5e4d443fe04c51c177c233b545919fc596
Daryll M <djtm97@gmail.com>
2022-01-22 23:03:12 -0500

    fix about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-// import coverImage from '../../assets/cover/cover-image.jpg';
+import selfieImage from '../../assets/images/Daryll-Munoz-Selfie.jpg';
+
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">About Me</h1>
-      {/* <img src="assets/images/Daryll-Munoz-Selfie.jpg" alt="Daryll Munoz Selfie"> */}
-      <div className="my-2">
-        <p>
-        Daryll is currently a business analyst based in the New York Metro area with experience in user-acceptance testing, automation testing, and the cloud.
-        He graduated with a B.S. in Chemical Engineering with highest honors from Rutgers University and is currently enrolled in Rutgers Full Stack Coding Bootcamp.
-        He aspires to be an effective full-stack developer proficient in current technologies. Outside of work, Daryll enjoys traveling, learning languages, playing instruments, and spending time with family and friends.
-        </p>
-      </div>
+    <section id="About-Me" class="aboutme">
+      <img src={selfieImage} alt="Daryll Munoz Selfie"/>
+        <div class="aboutmecontainer">
+          <h2 class="section-title primary-border">
+            About Me
+          </h2>
+          <div class="text">
+            <p>
+              Daryll is currently a business analyst based in the New York Metro area with experience in
+              user-acceptance testing, automation testing, and the cloud. He is currently staffed as a QA Engineer
+              at one of the nation's largest telecommunication companies in their customer experience domain.
+            </p>
+            <p>
+              He graduated with a B.S. in Chemical Engineering with highest honors from Rutgers University and is
+              currently enrolled in Rutgers Full Stack Coding Bootcamp.
+            </p>
+            <p>
+              He aspires to be an effective full-stack developer proficient in current technologies.
+              Outside of work, Daryll enjoys traveling, learning languages, playing instruments, and spending time
+              with family and friends.
+            </p>
+          </div>
+        </div>
     </section>
   );
 }
